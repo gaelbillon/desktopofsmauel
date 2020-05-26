5cfe76f0ff6ea706b71c2003b96d0d026e90f05c
gaelbillon <gael.billon@gmail.com>
2020-05-26 11:47:07 +0200

    Added Commento.io

--- src/components/PostTemplate.jsx
+++ src/components/PostTemplate.jsx
@@ -4,6 +4,7 @@ import { MDXRenderer } from "gatsby-plugin-mdx";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Img from "gatsby-image";
+import Commento from "./Commento";
 
 const Container = styled.main`
   h1 {
@@ -18,11 +19,14 @@ class PostTemplate extends React.Component {
     const post = postNode.frontmatter;
 
     return (
-      <Container>
-        <Img sizes={postNode.frontmatter.cover.childImageSharp.sizes} />
-        <h1>{post.title}</h1>
-        <MDXRenderer>{postNode.body}</MDXRenderer>
-      </Container>
+      <div>
+        <Container>
+          <Img sizes={postNode.frontmatter.cover.childImageSharp.sizes} />
+          <h1>{post.title}</h1>
+          <MDXRenderer>{postNode.body}</MDXRenderer>
+        </Container>
+        <Commento />
+      </div>
     );
   }
 }
